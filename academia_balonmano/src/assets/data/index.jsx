3df37acf2cbc07eea8b62adb352aa12edf96cf53
Maria Suarez <msuarezh@gmail.com>
2023-08-30 11:37:10 +0200

    data array modified

--- academia_balonmano/src/assets/data/index.jsx
+++ academia_balonmano/src/assets/data/index.jsx
@@ -22,7 +22,103 @@ const data = [
                 text: "Un atacante solo colabora con bloqueos y pase y se va. No lanza."
         }
         ]
-    }
+    },
+    {
+        id: 3002,
+        group: ["juego", "global"],
+        name:"Gol al bote",
+        text: "Crear espacios sin balón",
+        categories: ["juego", "gol al bote"],
+        content: ["desmarque, marcaje", "campo visual", "petición", "relación"],
+        players: {
+            attack: "Pase sobre línea es gol si recepciona compañero",
+            defense: "Intercepta para atacar",
+            shoot: "Elegir pase de gol o continuidad"
+        },
+        tacticals: {
+            attack: "Desmarque para crear espacios",
+            defense: "Ver oponente y balón",
+            shoot: "Prever la siguiente acción antes de recibir",
+        },
+        variants: [
+            {
+                name:"Objetivos ofensivos",
+                text: "Un atacante solo colabora con bloqueos y pase y se va. No lanza."
+        }
+        ]
+    },
+    {
+        id: 3002,
+        group: ["juego", "global"],
+        name:"Gol al bote",
+        text: "Crear espacios sin balón",
+        categories: ["juego", "gol al bote"],
+        content: ["desmarque, marcaje", "campo visual", "petición", "relación"],
+        players: {
+            attack: "Pase sobre línea es gol si recepciona compañero",
+            defense: "Intercepta para atacar",
+            shoot: "Elegir pase de gol o continuidad"
+        },
+        tacticals: {
+            attack: "Desmarque para crear espacios",
+            defense: "Ver oponente y balón",
+            shoot: "Prever la siguiente acción antes de recibir",
+        },
+        variants: [
+            {
+                name:"Objetivos ofensivos",
+                text: "Un atacante solo colabora con bloqueos y pase y se va. No lanza."
+        }
+        ]
+    },
+    {
+        id: 3002,
+        group: ["juego", "global"],
+        name:"Gol al bote",
+        text: "Crear espacios sin balón",
+        categories: ["juego", "gol al bote"],
+        content: ["desmarque, marcaje", "campo visual", "petición", "relación"],
+        players: {
+            attack: "Pase sobre línea es gol si recepciona compañero",
+            defense: "Intercepta para atacar",
+            shoot: "Elegir pase de gol o continuidad"
+        },
+        tacticals: {
+            attack: "Desmarque para crear espacios",
+            defense: "Ver oponente y balón",
+            shoot: "Prever la siguiente acción antes de recibir",
+        },
+        variants: [
+            {
+                name:"Objetivos ofensivos",
+                text: "Un atacante solo colabora con bloqueos y pase y se va. No lanza."
+        }
+        ]
+    },
+    {
+        id: 3002,
+        group: ["juego", "global"],
+        name:"Gol al bote",
+        text: "Crear espacios sin balón",
+        categories: ["juego", "gol al bote"],
+        content: ["desmarque, marcaje", "campo visual", "petición", "relación"],
+        players: {
+            attack: "Pase sobre línea es gol si recepciona compañero",
+            defense: "Intercepta para atacar",
+            shoot: "Elegir pase de gol o continuidad"
+        },
+        tacticals: {
+            attack: "Desmarque para crear espacios",
+            defense: "Ver oponente y balón",
+            shoot: "Prever la siguiente acción antes de recibir",
+        },
+        variants: [
+            {
+                name:"Objetivos ofensivos",
+                text: "Un atacante solo colabora con bloqueos y pase y se va. No lanza."
+        }
+        ]
+    },
 ]
 
 export default data
